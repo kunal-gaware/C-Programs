b03d2d9a90f9824940739c265effdad7643d8892
kunal-gaware <55451090+kunal-gaware@users.noreply.github.com>
2023-11-22 12:41:46 +0530

    Add files via upload

---
+++ Print_Ascii_Value_of_the_Character.c
@@ -0,0 +1,16 @@
+// Print Ascii Value of the Character
+
+
+# include <stdio.h>
+
+int main()
+{
+    char c ;
+
+    printf("Enter a charecter to findi it's ascii value : ");
+    scanf("%c",&c);
+
+    printf (" ASCII Value of %c is %d\n", c, c);
+
+    return 0;
+}
